a365b8b232ff5a3d0a28dbe4453d4f55c9616825
Pitambar majhi <pitambar@Pitambars-MacBook-Air.local>
2025-12-09 21:52:00 +0530

    backend urlchange

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "http://101.53.148.53:8000/api",
 });
 
 export default API;
