ad113b03b887c0f45dc1769d16af8c9c720ecdd2
Pitambar majhi <pitambar@Pitambars-MacBook-Air.local>
2026-02-10 19:43:43 +0530

    package install for meta tag

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,10 +6,22 @@ import PopularDestinations from './PopularDestinations'
 import BookWithConfidence from './BookWithConfidence'
 import KilimanjaroTrekking from './KilimanjaroTrekking'
 import PopularWay from './PopularWay'
+import SEO from '../../components/SEO'
+
+
 
 const Home = () => {
+
+  
   return (
     <div>
+      <SEO
+      title="Imaraki Leleni Safaris - Local Tanzania Safari & Kilimanjaro Specialists"
+      description="Local experts creating personalised Tanzania safaris and Kilimanjaro climbs with honest guidance."
+      keywords="Tanzania safari, Kilimanjaro climb, local safari experts"
+      image="https://imarakilelenisafaris.com/assets/tanzania-banner-5.webp"
+      url="https://imarakilelenisafaris.com/"
+    />
 <HeroSlider/>
 <BookedTrips/>
 <PopularDestinations/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+   <HelmetProvider>
     <App />
-  </StrictMode>,
+  </HelmetProvider>,
 )
