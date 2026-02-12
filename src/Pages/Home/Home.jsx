ba40894f0b2ae2ce428eb682640656eff0ba2bf1
Pitambar majhi <pitambar@Pitambars-MacBook-Air.local>
2026-02-12 11:50:49 +0530

    package install for un install

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,7 +6,7 @@ import PopularDestinations from './PopularDestinations'
 import BookWithConfidence from './BookWithConfidence'
 import KilimanjaroTrekking from './KilimanjaroTrekking'
 import PopularWay from './PopularWay'
-import SEO from '../../components/SEO'
+// import SEO from '../../components/SEO'
 
 
 
@@ -15,13 +15,13 @@ const Home = () => {
   
   return (
     <div>
-      <SEO
+      {/* <SEO
       title="Imaraki Leleni Safaris - Local Tanzania Safari & Kilimanjaro Specialists"
       description="Local experts creating personalised Tanzania safaris and Kilimanjaro climbs with honest guidance."
       keywords="Tanzania safari, Kilimanjaro climb, local safari experts"
       image="https://imarakilelenisafaris.com/assets/tanzania-banner-5.webp"
       url="https://imarakilelenisafaris.com/"
-    />
+    /> */}
 <HeroSlider/>
 <BookedTrips/>
 <PopularDestinations/>
--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,38 +1,48 @@
-import { Helmet } from "react-helmet-async";
+// import { Helmet } from "react-helmet-async";
 
-const SEO = ({
-  title,
-  description,
-  keywords,
-  image,
-  url,
-  type = "website",
-}) => {
+// const SEO = ({
+//   title,
+//   description,
+//   keywords,
+//   image,
+//   url,
+//   type = "website",
+// }) => {
+//   return (
+//     <Helmet>
+//       {/* Basic SEO */}
+//       <title>{title}</title>
+//       <meta name="description" content={description} />
+//       {keywords && <meta name="keywords" content={keywords} />}
+
+//       {/* Open Graph */}
+//       <meta property="og:type" content={type} />
+//       <meta property="og:title" content={title} />
+//       <meta property="og:description" content={description} />
+//       <meta property="og:url" content={url} />
+//       <meta property="og:image" content={image} />
+//       <meta property="og:image:secure_url" content={image} />
+//       <meta property="og:site_name" content="Imaraki Leleni Safaris" />
+//       <meta property="og:image:width" content="1200" />
+//       <meta property="og:image:height" content="630" />
+
+//       {/* Twitter */}
+//       <meta name="twitter:card" content="summary_large_image" />
+//       <meta name="twitter:title" content={title} />
+//       <meta name="twitter:description" content={description} />
+//       <meta name="twitter:image" content={image} />
+//     </Helmet>
+//   );
+// };
+
+// export default SEO;
+
+import React from 'react'
+
+const SEO = () => {
   return (
-    <Helmet>
-      {/* Basic SEO */}
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      {keywords && <meta name="keywords" content={keywords} />}
+    <div>SEO</div>
+  )
+}
 
-      {/* Open Graph */}
-      <meta property="og:type" content={type} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:url" content={url} />
-      <meta property="og:image" content={image} />
-      <meta property="og:image:secure_url" content={image} />
-      <meta property="og:site_name" content="Imaraki Leleni Safaris" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-
-      {/* Twitter */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
-    </Helmet>
-  );
-};
-
-export default SEO;
+export default SEO
